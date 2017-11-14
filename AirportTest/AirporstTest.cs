3f47be5d5f288bdb2207394d078579dbf07f2ffe
danielill324 <danielill324@gmail.com>
2017-11-14 23:29:48 +0200

    first commit

---
+++ AirportTest/AirporstTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using airportManager;
+
+namespace AirportTest
+{
+    [TestClass]
+    public class AirporstTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            bool actualVal = managerTest.find_place(new Plane(1));
+
+            //act
+            bool expected = true;
+
+            //assert
+            Assert.AreEqual(expected, actualVal);
+            
+        }
+
+    }
+}
---
+++ airportManagerTests/MainWindowTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using airportManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airportManager.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSelectedCellValueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
