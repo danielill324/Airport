cdbe9ad787324895b5ff5534f95981cff3ddbe21
danielill324 <danielill324@gmail.com>
2017-11-15 01:11:36 +0200

    first commit

--- AirportTest/AirporstTest.cs
+++ AirportTest/AirporstTest.cs
@@ -8,7 +8,7 @@ namespace AirportTest
     public class AirporstTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void find_place()//check if the method return true when there is place for plane
         {
             //arrange
             Manager managerTest = new Manager();
@@ -21,6 +21,98 @@ namespace AirportTest
             Assert.AreEqual(expected, actualVal);
             
         }
+        [TestMethod]
+        public void getPlaneById()//check if there isnt such id in the list of planes returns null
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            Plane actualVal = managerTest.getPlaneById("plane id");
 
+            //act
+            Plane expected = null;
+
+            //assert
+            Assert.IsNull(expected);
+
+        }
+        [TestMethod]
+        public void getPlaneById2()//check if the rigth object returns by his given id
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            Plane plane = new Plane(1);
+            managerTest.planeList.Add(plane);
+            Plane actualVal = managerTest.getPlaneById(plane.id);
+
+            //act
+            Plane expected = plane ;
+
+            //assert
+            Assert.ReferenceEquals(expected,actualVal);
+
+        }
+        [TestMethod]
+        public void removePlane()//check if the rigth object returns by his given id
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            
+            bool actualVal = managerTest.removePlane("id");
+
+            //act
+            
+
+            //assert
+            Assert.IsFalse(actualVal);
+
+        }
+        [TestMethod]
+        public void removePlane2()//check if the rigth object returns by his given id
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            Plane p = new Plane(2);
+            managerTest.find_place(p);
+            managerTest.planeList.Add(p);
+            bool actualVal = managerTest.removePlane(p.id);
+
+            //act
+
+
+            //assert
+            Assert.IsTrue(actualVal);
+
+        }
+        [TestMethod]
+        public void remove_plane()//check if the rigth object returns by his given id
+        {
+            //arrange
+            Manager managerTest = new Manager();
+            Plane p = new Plane(2);
+            managerTest.find_place(p);
+            managerTest.planeList.Add(p);
+            bool actualVal = managerTest.remove_plane(p);
+
+            //act
+
+
+            //assert
+            Assert.IsTrue(actualVal);
+
+        }
+        public void remove_plane2()//check if the rigth object returns by his given id
+        {
+            //arrange
+            Manager managerTest = new Manager();
+
+            bool actualVal = managerTest.remove_plane(new Plane(2));
+
+            //act
+
+
+            //assert
+            Assert.IsFalse(actualVal);
+
+        }
     }
 }
---
+++ airportManagerTests/HangarTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using airportManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace airportManager.Tests
+{
+    [TestClass()]
+    public class HangarTests
+    { 
+        [TestMethod()]
+        public void isFullTest()
+        {
+            //assert 
+            Hangar hangar = new Hangar(4);
+            hangar.addPlane(new Plane(2));
+            bool actualval = hangar.isFull();
+            //Assert
+            Assert.IsFalse(actualval);
+        }
+
+        [TestMethod()]
+        public void isFullTest2()
+        {
+            //assert 
+            Hangar hangar = new Hangar(4);
+            hangar.addPlane(new Plane(4));
+            bool actualval = hangar.isFull();
+            //Assert
+            Assert.IsTrue(actualval);
+        }
+        [TestMethod()]
+        public void hangar_total_place()
+        {
+            //arrange
+            Hangar hangarTest = new Hangar(4);
+            hangarTest.addPlane(new Plane(2));
+            int actualval = hangarTest.place_sum();
+
+            //act
+            int expected = 2;
+            Assert.AreEqual(expected, actualval);
+        }
+        [TestMethod()]
+        public void addPlaneTest()
+        {
+            //arrange
+            Hangar hangarTest = new Hangar(1);
+            Plane p = new Plane(4);
+            bool actualval = hangarTest.addPlane(p);
+            //assert
+            Assert.IsFalse(actualval);
+        }
+        public void addPlaneTest2()
+        {
+            //arrange
+            Hangar hangarTest = new Hangar(4);
+            Plane p = new Plane(1);
+            bool actualval = hangarTest.addPlane(p);
+            //assert
+            Assert.IsTrue(actualval);
+        }
+        [TestMethod()]
+        public void find_planeTest()
+        {
+            //arrange
+            Plane p = new Plane(1);
+            Hangar hangarTest = new Hangar(2);
+            bool actualVal = hangarTest.find_plane(p);
+            //assert
+            Assert.IsFalse(actualVal);
+        }
+
+        [TestMethod()]
+        public void find_planeTest2()
+        {
+            //arrange
+            Plane p = new Plane(1);
+            Hangar hangarTest = new Hangar(2);
+            bool actualVal = hangarTest.find_plane(p);
+            //assert
+            Assert.IsFalse(actualVal);
+        }
+        [TestMethod()]
+        public void remove_planeTest()
+        {
+            //arrange
+            Plane p = new Plane(2);
+            Hangar hangarTest = new Hangar(2);
+            bool actualVal = hangarTest.remove_plane(p);
+
+            //act
+
+
+            //assert
+            Assert.IsFalse(actualVal);
+        }
+        public void remove_planeTest2()
+        {
+            //arrange
+            Plane p = new Plane(2);
+            Hangar hangarTest = new Hangar(2);
+            hangarTest.addPlane(p);
+            bool actualVal = hangarTest.remove_plane(p);
+
+            //act
+
+
+            //assert
+            Assert.IsTrue(actualVal);
+        }
+
+    }
+}
